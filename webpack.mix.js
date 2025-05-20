0f14d030937413b67c327a1beb52099a2c201a66
Swapnajit Sahoo <sj422159@gmail.com>
2025-05-20 10:06:22 +0530

    Fix npm build for Railway deployment

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+const mix = require('laravel-mix');
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+   .postCss('resources/css/app.css', 'public/css', [
+       require('postcss-import'),
+       require('tailwindcss'),
+       require('autoprefixer'),
+   ]);
