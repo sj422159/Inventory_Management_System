b64ecd60b58ffd3c01cecab46971277cb65957a9
Swapnajit Sahoo <sj422159@gmail.com>
2025-05-20 23:02:47 +0530

    css fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,10 +7,10 @@
     <title>Inventory-Management-System</title>
 
     <!-- Font Icon -->
-    <link rel="stylesheet" href="{{ asset('frontend') }}/fonts/material-icon/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="{{ asset('frontend/fonts/material-icon/css/material-design-iconic-font.min.css') }}">
 
     <!-- Main css -->
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/style.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
 </head>
 <body>
 
